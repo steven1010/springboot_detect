6ba764c40d93a58bc51c925cdd73d0244b993df5
pengshanglin <pengshanglin@skspurce.net>
2017-09-08 11:06:50 +0800

    project init

---
+++ detect-webapi/src/main/java/com/skspruce/ism/detect/webapi/App.java
@@ -0,0 +1,17 @@
+package com.skspruce.ism.detect.webapi;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(App.class, args);
+
+    }
+}
