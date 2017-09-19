f8a6e757b638c4b16afa4a1f4aee8074696c1bac
Tengfei Wang <tengfeiwang@skspruce.net>
2017-09-19 17:45:26 +0800

    mysql多数据源

---
+++ detect-webapi/src/main/java/com/skspruce/ism/detect/webapi/strategy/repository/StrategyJpaRepository.java
@@ -0,0 +1,20 @@
+package com.skspruce.ism.detect.webapi.strategy.repository;
+
+import com.skspruce.ism.detect.webapi.strategy.entity.Strategy;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+public interface StrategyJpaRepository extends JpaRepository<Strategy, Integer>,CrudRepository<Strategy,Integer> {
+
+    Strategy findStrategyById(Integer id);
+
+    @Transactional
+    @Query(nativeQuery = true, value = "DELETE FROM strategy where id in (:ids)")
+    @Modifying
+    void deleteByIds(@Param(value = "ids") Integer[] ids);
+}
